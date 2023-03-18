4060e31a9f4e5e5798ed03be286698ec65e931a2
Pablo Zhurbickiy <pavelkortp@gmail.com>
2023-03-18 19:37:15 +0200

    lesson 17 dop

--- alishevcourse/src/main/java/org/pavelkortp/springcourse/controllers/FirstController.java
+++ alishevcourse/src/main/java/org/pavelkortp/springcourse/controllers/FirstController.java
@@ -3,8 +3,10 @@ package org.pavelkortp.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
